96c89c4c9cd7344b614986af882f226c7062cffc
qw1xwr1 <oxenblokxozetc@gmail.com>
2025-06-28 19:48:56 +0300

    Update ChatPatches.cs

--- src/ChatPatches.cs
+++ src/ChatPatches.cs
@@ -6,32 +6,24 @@ namespace AUnlocker;
 [HarmonyPatch(typeof(ChatController), nameof(ChatController.Update))]
 public static class ChatJailbreak_ChatController_Update_Postfix
 {
-    /// <summary>
-    /// Remove the chat cooldown and the character limit.
-    /// </summary>
-    /// <param name="__instance">The <c>ChatController</c> instance.</param>
     public static void Postfix(ChatController __instance)
     {
-        // [UNSAFE] No Chat Cooldown
         if (AUnlocker.NoChatCooldown.Value)
         {
-            // if (!__instance.freeChatField.textArea.hasFocus) return;
             __instance.timeSinceLastMessage = 3f;
         }
 
-        // [UNSAFE] No Character Limit
         if (AUnlocker.NoCharacterLimit.Value)
         {
             __instance.freeChatField.textArea.characterLimit = int.MaxValue;
         }
-
         else if (AUnlocker.PatchChat.Value)
         {
             __instance.freeChatField.textArea.AllowPaste = true;
             __instance.freeChatField.textArea.AllowSymbols = true;
             __instance.freeChatField.textArea.AllowEmail = true;
             __instance.freeChatField.textArea.allowAllCharacters = true;
-            __instance.freeChatField.textArea.characterLimit = 120;  // above 120 characters anti-cheat will kick you
+            __instance.freeChatField.textArea.characterLimit = 120;
         }
     }
 }
@@ -39,39 +31,26 @@ public static class ChatJailbreak_ChatController_Update_Postfix
 [HarmonyPatch(typeof(FreeChatInputField), nameof(FreeChatInputField.UpdateCharCount))]
 public static class EditColorIndicators_FreeChatInputField_UpdateCharCount_Postfix
 {
-    /// <summary>
-    /// Update the character count color indicator based on the current text length.
-    /// </summary>
-    /// <param name="__instance">The <c>FreeChatInputField</c> instance.</param>
     public static void Postfix(FreeChatInputField __instance)
     {
         if (AUnlocker.NoCharacterLimit.Value)
         {
             var length = __instance.textArea.text.Length;
-            // Show new character limit below text field
             __instance.charCountText.SetText($"{length}/{__instance.textArea.characterLimit}");
-
             __instance.charCountText.color = length switch
             {
-                // Black if not close to limit (under 75%)
                 < 1610612735 => Color.black,
-                // Yellow if close to limit (under 100%)
                 < 2147483647 => new Color(1f, 1f, 0f, 1f),
                 _ => Color.red
             };
         }
-
         else if (AUnlocker.PatchChat.Value)
         {
             var length = __instance.textArea.text.Length;
-            // Show new character limit below text field
             __instance.charCountText.SetText($"{length}/{__instance.textArea.characterLimit}");
-
             __instance.charCountText.color = length switch
             {
-                // Black if not close to limit (under 75%)
                 < 90 => Color.black,
-                // Yellow if close to limit (under 100%)
                 < 120 => new Color(1f, 1f, 0f, 1f),
                 _ => Color.red
             };
@@ -82,11 +61,6 @@ public static class EditColorIndicators_FreeChatInputField_UpdateCharCount_Postf
 [HarmonyPatch(typeof(ChatController), nameof(ChatController.SendFreeChat))]
 public static class AllowURLS_ChatController_SendFreeChat_Prefix
 {
-    /// <summary>
-    /// Remove the URL filtering when sending a chat message.
-    /// </summary>
-    /// <param name="__instance">The <c>ChatController</c> instance.</param>
-    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
     public static bool Prefix(ChatController __instance)
     {
         if (!AUnlocker.PatchChat.Value) return true;
@@ -101,22 +75,15 @@ public static class AllowURLS_ChatController_SendFreeChat_Prefix
 [HarmonyPatch(typeof(TextBoxTMP), nameof(TextBoxTMP.IsCharAllowed))]
 public static class AllowAllCharacters_TextBoxTMP_IsCharAllowed_Prefix
 {
-    /// <summary>
-    /// Allow any character to be typed into the chatbox.
-    /// </summary>
-    /// <param name="__instance">The <c>TextBoxTMP</c> instance.</param>
-    /// <param name="__result">Original return value of <c>IsCharAllowed</c>.</param>
-    /// <param name="i"> The character to check.</param>
-    /// <returns><c>false</c> to skip the original method, <c>true</c> to allow the original method to run.</returns>
     public static bool Prefix(TextBoxTMP __instance, ref bool __result, char i)
     {
-        // Original game code:
-        // public bool IsCharAllowed(char i)
-        // {
-        //   return this.IpMode ? i >= '0' && i <= '9' || i == '.' : i == ' ' || i >= 'A' && i <= 'Z' || i >= 'a' && i <= 'z' || i >= '0' && i <= '9' || i >= 'À' && i <= 'ÿ' || i >= 'Ѐ' && i <= 'џ' || i >= '\u3040' && i <= '㆟' || i >= 'ⱡ' && i <= '힣' || this.AllowSymbols && TextBoxTMP.SymbolChars.Contains(i) || this.AllowEmail && TextBoxTMP.EmailChars.Contains(i);
-        // }
-
         if (!AUnlocker.AllowAllCharacters.Value) return true;
+
+        if (i == 'о' || i == 'м') // <-- Разрешаем русские буквы "о" и "м"
+        {
+            __result = true;
+            return false;
+        }
 
         if (i is >= 'À' and <= 'ÿ')
         {
@@ -148,28 +115,12 @@ public static class AllowAllCharacters_TextBoxTMP_IsCharAllowed_Prefix
             __result = true;
             return false;
         }
-        // Bugfix: backspace messing with chat message;
-        // newline / "enter" to prevent message sending "randomly" (see issue #25)
         if (i is '\b' or '\n' or '\r')
         {
             __result = false;
             return false;
         }
 
-        // // logging
-        // string charRepresentation = i switch
-        // {
-        //     '\b' => "\\b",
-        //     '\n' => "\\n",
-        //     '\r' => "\\r",
-        //     _ => i.ToString()
-        // };
-
-        // Debug.Log($"IsCharAllowed({charRepresentation}) (Unicode: {(int)i}) = {__result}");
-
-        // accept any other character by default (including emojis, special characters, etc.)
-        // this can cause issues where we would have to deny certain characters
-        // that are messing with the chatbox (like we saw in issues #25 and #31)
         __result = true;
         return false;
     }
@@ -178,15 +129,11 @@ public static class AllowAllCharacters_TextBoxTMP_IsCharAllowed_Prefix
 [HarmonyPatch(typeof(TextBoxTMP), nameof(TextBoxTMP.Start))]
 public static class AllowPaste_TextBoxTMP_Start_Postfix
 {
-    /// <summary>
-    /// Allow email symbols to be typed into the chatbox and enables pasting text with CTRL + V.
-    /// </summary>
-    /// <param name="__instance">The <c>TextBoxTMP</c> instance.</param>
     public static void Postfix(TextBoxTMP __instance)
     {
         if (!AUnlocker.PatchChat.Value) return;
 
-        __instance.allowAllCharacters = true; // not used by game's code, but I include it anyway
+        __instance.allowAllCharacters = true;
         __instance.AllowEmail = true;
         __instance.AllowPaste = true;
         __instance.AllowSymbols = true;
@@ -196,10 +143,6 @@ public static class AllowPaste_TextBoxTMP_Start_Postfix
 [HarmonyPatch(typeof(TextBoxTMP), nameof(TextBoxTMP.Update))]
 public static class AllowCopy_TextBoxTMP_Update_Postfix
 {
-    /// <summary>
-    /// Allow copying text from the chatbox to the device's clipboard when pressing CTRL + C.
-    /// </summary>
-    /// <param name="__instance">The <c>TextBoxTMP</c> instance.</param>
     public static void Postfix(TextBoxTMP __instance)
     {
         if (!AUnlocker.PatchChat.Value || !__instance.hasFocus) return;
